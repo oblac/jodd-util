32a52cf586ebd0c40d55e5c0101d96d9a6cd31d6
Igor Spasić <igor.spasic@gmail.com>
2022-12-08 17:04:56 +0100

    Use floor() rather the round().

--- src/main/java/jodd/time/JulianDate.java
+++ src/main/java/jodd/time/JulianDate.java
@@ -365,8 +365,8 @@ public class JulianDate implements Serializable, Cloneable {
 
 		double d_millis = (d_second - (double)time_second) * 1000.0;
 
-		// round to the nearest millisecond
-		final int time_millisecond = (int) Math.round(d_millis);
+		// floor down to the nearest millisecond
+		final int time_millisecond = (int) Math.floor(d_millis);
 
 		return LocalDateTime.of(time_year, time_month, time_day, time_hour, time_minute, time_second)
 				.plusNanos(time_millisecond * 1000000L);
